64d30a1f41c33e4f0a11ed96a6875104ee3eb43e
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2020-11-03 11:55:23 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ light.set_pixel_color(5, light.rgb(0,255,255))
 
 light.set_brightness(10)
 light.set_pixel_color(0, light.rgb(255,100,0))
-light.set_pixel_color(1, light.rgb(245, 239, 66))
+light.set_pixel_color(1, light.rgb(245, 255, 0))
 light.set_pixel_color(2, light.rgb(150, 245, 66))
-light.set_pixel_color(3, light.rgb(66, 245, 227))
+light.set_pixel_color(3, light.rgb(655, 189, 61))
 light.set_pixel_color(4, light.rgb(103,181,199))
 light.set_pixel_color(5, light.rgb(66, 188, 245))
 light.set_pixel_color(6, light.rgb(80,123,242))
 light.set_pixel_color(7, light.rgb(190, 95, 227))
 light.set_pixel_color(8, light.rgb(245,66,212))
-light.set_pixel_color(9, light.rgb(248,48,78))
+light.set_pixel_color(9, light.rgb(242,48,78))
 pause(2000)
 light.clear() 
 pause(3000) 
@@ -29,11 +29,3 @@ light.set_pixel_color(6, light.rgb(80,123,242))
 light.set_pixel_color(7, light.rgb(190, 95, 227))
 light.set_pixel_color(8, light.rgb(245,66,212))
 light.set_pixel_color(9, light.rgb(248,48,78))
-
-
-
-
-
-
-
-
